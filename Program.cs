08616f23ddbcb97aebdcf56eea3176d567f9fa2f
Ali Salmeh <alisalmeh18@gmail.com>
2023-08-12 11:20:11 +0330

    add initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AliSalmeh_ProjectWeek5.TV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            bool nextChannel = false;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"**Channel {++i}**");
+                Console.WriteLine("Do you like this Channel? (yes/no)");
+                string userAns = Console.ReadLine().ToLower();
+
+                nextChannel = userAns == "yes" ? true : false;
+
+            } while (nextChannel == false);
+
+            Console.WriteLine($"Enjoy watching channel {i} ");
+            Console.WriteLine("^__^");
+
+            /* string userAnswer = null;
+            int channel = 0;
+            while (userAnswer != "yes")
+            {
+                Console.WriteLine($"DO you love channel {++channel}?");
+                userAnswer = Console.ReadLine();
+            } */
+        }
+    }
+}
